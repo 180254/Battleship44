8dc28a159fd509e46350889f9a5ff78234124afa
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-08-05 09:02:07 +0200

    tests for neighboursX and for custom assertions

--- src/test/java/pl/nn44/battleship/model/GridTests.java
+++ src/test/java/pl/nn44/battleship/model/GridTests.java
@@ -7,91 +7,82 @@ import java.util.List;
 
 public class GridTests {
 
+    private final int[] CELLS_23 = {
+            0, 1,
+            1, 0,
+            0, 1
+    };
+    private final int[] CELLS_33 = {
+            0, 1, 0,
+            1, 0, 1,
+            0, 1, 0,
+    };
+    private final int[] CELLS_34 = {
+            1, 0, 1,
+            0, 1, 0,
+            1, 0, 1,
+            0, 1, 0
+    };
+    private final int[] CELLS_43 = {
+            0, 0, 0, 1,
+            1, 1, 0, 0,
+            0, 0, 0, 1
+    };
+
     @Test
     public void newGrid_properSize_square() {
-        int[] cells = {
-                0, 0, 0,
-                1, 1, 0,
-                1, 0, 1,
-        };
+        int[] cells = CELLS_33.clone();
         new Grid(3, 3, cells);
     }
 
     @Test
     public void newGrid_properSize_notSquare() {
-        int[] cells = {
-                0, 0, 0,
-                1, 1, 0,
-                1, 0, 1,
-                0, 0, 0
-        };
+        int[] cells = CELLS_34.clone();
         new Grid(3, 4, cells);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void newGrid_wrongSize_square() {
-        int[] cells = {
-                0, 0, 0,
-                1, 1, 0,
-                1, 0, 1,
-        };
+        int[] cells = CELLS_33.clone();
         new Grid(3, 4, cells);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void newGrid_wrongSize_notSquare() {
-        int[] cells = {
-                0, 0, 0,
-                1, 1, 0,
-                1, 0, 1,
-                0, 0, 0, 2
-        };
+        int[] cells = CELLS_34.clone();
         new Grid(3, 5, cells);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void newGrid_wrongSize_zeroX() {
-        int[] cells = {
-                0, 0, 0,
-                1, 1, 0,
-        };
+        int[] cells = CELLS_33.clone();
         new Grid(0, 2, cells);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void newGrid_wrongSize_zeroY() {
-        int[] cells = {
-                0, 0, 0,
-                1, 1, 0,
-        };
+        int[] cells = CELLS_33.clone();
         new Grid(3, 0, cells);
     }
 
-
     @Test
     public void getCell_first() {
-        int[] cells = {
-                1, 0, 0,
-                0, 0, 0,
-        };
-        Grid grid = new Grid(3, 2, cells);
+        int[] cells = CELLS_33.clone();
+        Grid grid = new Grid(3, 3, cells);
 
         Coord cellCord = new Coord(0, 0);
         Cell cell = grid.getCell(cellCord);
 
-        Assert.assertSame(Cell.Type.getByCode(1), cell.getType());
+        Assert.assertSame(Cell.Type.getByCode(0), cell.getType());
         Assert.assertSame(cellCord, cell.getCoord());
     }
 
     @Test
     public void getCell_last() {
-        int[] cells = {
-                0, 0, 0,
-                0, 0, 1,
-        };
-        Grid grid = new Grid(3, 2, cells);
+        int[] cells = CELLS_23.clone();
+        Grid grid = new Grid(2, 3, cells);
 
-        Coord cellCord = new Coord(2, 1);
+        Coord cellCord = new Coord(1, 2);
         Cell cell = grid.getCell(cellCord);
 
         Assert.assertSame(Cell.Type.getByCode(1), cell.getType());
@@ -100,15 +91,10 @@ public class GridTests {
 
     @Test
     public void getCell_lastInRow() {
-        int[] cells = {
-                0, 0, 0,
-                0, 0, 1,
-                0, 0, 0,
-                0, 0, 0,
-        };
+        int[] cells = CELLS_34.clone();
         Grid grid = new Grid(3, 4, cells);
 
-        Coord cellCord = new Coord(2, 1);
+        Coord cellCord = new Coord(2, 2);
         Cell cell = grid.getCell(cellCord);
 
         Assert.assertSame(Cell.Type.getByCode(1), cell.getType());
@@ -117,14 +103,22 @@ public class GridTests {
 
     @Test
     public void getCell_firstInRow() {
-        int[] cells = {
-                0, 0, 0,
-                1, 0, 0,
-                0, 0, 0,
-        };
-        Grid grid = new Grid(3, 3, cells);
+        int[] cells = CELLS_34.clone();
+        Grid grid = new Grid(3, 4, cells);
 
         Coord cellCord = new Coord(0, 1);
+        Cell cell = grid.getCell(cellCord);
+
+        Assert.assertSame(Cell.Type.getByCode(0), cell.getType());
+        Assert.assertSame(cellCord, cell.getCoord());
+    }
+
+    @Test
+    public void getCell_some() {
+        int[] cells = CELLS_34.clone();
+        Grid grid = new Grid(3, 4, cells);
+
+        Coord cellCord = new Coord(1, 1);
         Cell cell = grid.getCell(cellCord);
 
         Assert.assertSame(Cell.Type.getByCode(1), cell.getType());
@@ -133,11 +127,7 @@ public class GridTests {
 
     @Test(expected = IllegalArgumentException.class)
     public void getCell_outOfGrid_X() {
-        int[] cells = {
-                0, 0, 0,
-                1, 0, 0,
-                0, 0, 0,
-        };
+        int[] cells = CELLS_33.clone();
         Grid grid = new Grid(3, 3, cells);
 
         Coord cellCord = new Coord(3, 0);
@@ -147,11 +137,7 @@ public class GridTests {
 
     @Test(expected = IllegalArgumentException.class)
     public void getCell_outOfGrid_Y() {
-        int[] cells = {
-                0, 0, 0,
-                1, 0, 0,
-                0, 0, 0,
-        };
+        int[] cells = CELLS_33.clone();
         Grid grid = new Grid(3, 3, cells);
 
         Coord cellCord = new Coord(0, 3);
@@ -160,11 +146,7 @@ public class GridTests {
 
     @Test(expected = IllegalArgumentException.class)
     public void getCell_minusX() {
-        int[] cells = {
-                0, 0, 0,
-                1, 0, 0,
-                0, 0, 0,
-        };
+        int[] cells = CELLS_33.clone();
         Grid grid = new Grid(3, 3, cells);
 
         Coord cellCord = new Coord(-1, 0);
@@ -173,11 +155,7 @@ public class GridTests {
 
     @Test(expected = IllegalArgumentException.class)
     public void getCell_minusY() {
-        int[] cells = {
-                0, 0, 0,
-                1, 0, 0,
-                0, 0, 0,
-        };
+        int[] cells = CELLS_33.clone();
         Grid grid = new Grid(3, 3, cells);
 
         Coord cellCord = new Coord(0, -1);
@@ -186,11 +164,7 @@ public class GridTests {
 
     @Test
     public void getNeighboursPlus_allInGrid() {
-        int[] cells = {
-                0, 0, 0,
-                1, 1, 0,
-                0, 0, 0,
-        };
+        int[] cells = CELLS_33.clone();
         Grid grid = new Grid(3, 3, cells);
 
         Coord coord = new Coord(1, 1);
@@ -206,11 +180,7 @@ public class GridTests {
 
     @Test
     public void getNeighboursPlus_someOutOfGrid_top() {
-        int[] cells = {
-                0, 0, 0,
-                1, 1, 0,
-                0, 0, 0,
-        };
+        int[] cells = CELLS_33.clone();
         Grid grid = new Grid(3, 3, cells);
 
         Coord coord = new Coord(1, 0);
@@ -224,11 +194,7 @@ public class GridTests {
 
     @Test
     public void getNeighboursPlus_someOutOfGrid_left() {
-        int[] cells = {
-                0, 0, 0,
-                1, 1, 0,
-                0, 0, 0,
-        };
+        int[] cells = CELLS_33.clone();
         Grid grid = new Grid(3, 3, cells);
 
         Coord coord = new Coord(0, 1);
@@ -242,11 +208,7 @@ public class GridTests {
 
     @Test
     public void getNeighboursPlus_someOutOfGrid_right() {
-        int[] cells = {
-                0, 0, 0,
-                1, 1, 0,
-                0, 0, 0,
-        };
+        int[] cells = CELLS_33.clone();
         Grid grid = new Grid(3, 3, cells);
 
         Coord coord = new Coord(2, 1);
@@ -260,11 +222,7 @@ public class GridTests {
 
     @Test
     public void getNeighboursPlus_someOutOfGrid_bottom() {
-        int[] cells = {
-                0, 0, 0,
-                1, 1, 0,
-                0, 0, 0,
-        };
+        int[] cells = CELLS_33.clone();
         Grid grid = new Grid(3, 3, cells);
 
         Coord coord = new Coord(1, 2);
@@ -278,11 +236,7 @@ public class GridTests {
 
     @Test
     public void getNeighboursPlus_someOutOfGrid_topLeft() {
-        int[] cells = {
-                0, 0, 0, 1,
-                1, 1, 0, 0,
-                0, 0, 0, 1
-        };
+        int[] cells = CELLS_43.clone();
         Grid grid = new Grid(4, 3, cells);
 
         Coord coord = new Coord(0, 0);
@@ -295,11 +249,7 @@ public class GridTests {
 
     @Test
     public void getNeighboursPlus_someOutOfGrid_topRight() {
-        int[] cells = {
-                0, 0, 0, 1,
-                1, 1, 0, 0,
-                0, 0, 0, 1
-        };
+        int[] cells = CELLS_43.clone();
         Grid grid = new Grid(4, 3, cells);
 
         Coord coord = new Coord(3, 0);
@@ -312,11 +262,7 @@ public class GridTests {
 
     @Test
     public void getNeighboursPlus_someOutOfGrid_bottomLeft() {
-        int[] cells = {
-                0, 0, 0, 1,
-                1, 1, 0, 0,
-                0, 0, 0, 1
-        };
+        int[] cells = CELLS_43.clone();
         Grid grid = new Grid(4, 3, cells);
 
         Coord coord = new Coord(0, 2);
@@ -329,11 +275,7 @@ public class GridTests {
 
     @Test
     public void getNeighboursPlus_someOutOfGrid_bottomRight() {
-        int[] cells = {
-                0, 0, 0, 1,
-                1, 1, 0, 0,
-                0, 0, 0, 1
-        };
+        int[] cells = CELLS_43.clone();
         Grid grid = new Grid(4, 3, cells);
 
         Coord coord = new Coord(3, 2);
@@ -343,4 +285,122 @@ public class GridTests {
         Assert.assertTrue(neighbours.contains(grid.getCell(new Coord(3, 1))));
         Assert.assertTrue(neighbours.contains(grid.getCell(new Coord(2, 2))));
     }
+
+    @Test
+    public void getNeighboursX_allInGrid() {
+        int[] cells = CELLS_43.clone();
+        Grid grid = new Grid(4, 3, cells);
+
+        Coord coord = new Coord(2, 1);
+        List<Cell> neighbours = grid.getNeighboursX(coord);
+        List<Coord> coordNeighbours = coord.neighboursX();
+
+        Assert.assertSame(4, neighbours.size());
+
+        for (Cell neighbour : neighbours) {
+            Assert.assertTrue(coordNeighbours.contains(neighbour.getCoord()));
+        }
+    }
+
+    @Test
+    public void getNeighboursX_someOutOfGrid_top() {
+        int[] cells = CELLS_43.clone();
+        Grid grid = new Grid(4, 3, cells);
+
+        Coord coord = new Coord(1, 0);
+        List<Cell> neighbours = grid.getNeighboursX(coord);
+
+        Assert.assertSame(2, neighbours.size());
+        Assert.assertTrue(neighbours.contains(grid.getCell(new Coord(0, 1))));
+        Assert.assertTrue(neighbours.contains(grid.getCell(new Coord(2, 1))));
+    }
+
+    @Test
+    public void getNeighboursX_someOutOfGrid_left() {
+        int[] cells = CELLS_43.clone();
+        Grid grid = new Grid(4, 3, cells);
+
+        Coord coord = new Coord(3, 1);
+        List<Cell> neighbours = grid.getNeighboursX(coord);
+
+        Assert.assertSame(2, neighbours.size());
+        Assert.assertTrue(neighbours.contains(grid.getCell(new Coord(2, 0))));
+        Assert.assertTrue(neighbours.contains(grid.getCell(new Coord(2, 2))));
+
+    }
+
+    @Test
+    public void getNeighboursX_someOutOfGrid_right() {
+        int[] cells = CELLS_43.clone();
+        Grid grid = new Grid(4, 3, cells);
+
+        Coord coord = new Coord(0, 1);
+        List<Cell> neighbours = grid.getNeighboursX(coord);
+
+        Assert.assertSame(2, neighbours.size());
+        Assert.assertTrue(neighbours.contains(grid.getCell(new Coord(1, 0))));
+        Assert.assertTrue(neighbours.contains(grid.getCell(new Coord(1, 2))));
+    }
+
+    @Test
+    public void getNeighboursX_someOutOfGrid_bottom() {
+        int[] cells = CELLS_43.clone();
+        Grid grid = new Grid(4, 3, cells);
+
+        Coord coord = new Coord(2, 2);
+        List<Cell> neighbours = grid.getNeighboursX(coord);
+
+        Assert.assertSame(2, neighbours.size());
+        Assert.assertTrue(neighbours.contains(grid.getCell(new Coord(1, 1))));
+        Assert.assertTrue(neighbours.contains(grid.getCell(new Coord(3, 1))));
+    }
+
+    @Test
+    public void getNeighboursX_someOutOfGrid_topLeft() {
+        int[] cells = CELLS_43.clone();
+        Grid grid = new Grid(4, 3, cells);
+
+        Coord coord = new Coord(0, 0);
+        List<Cell> neighbours = grid.getNeighboursX(coord);
+
+        Assert.assertSame(1, neighbours.size());
+        Assert.assertTrue(neighbours.contains(grid.getCell(new Coord(1, 1))));
+    }
+
+    @Test
+    public void getNeighboursX_someOutOfGrid_topRight() {
+        int[] cells = CELLS_43.clone();
+        Grid grid = new Grid(4, 3, cells);
+
+        Coord coord = new Coord(3, 0);
+        List<Cell> neighbours = grid.getNeighboursX(coord);
+
+        Assert.assertSame(1, neighbours.size());
+        Assert.assertTrue(neighbours.contains(grid.getCell(new Coord(2, 1))));
+    }
+
+    @Test
+    public void getNeighboursX_someOutOfGrid_bottomLeft() {
+        int[] cells = CELLS_43.clone();
+        Grid grid = new Grid(4, 3, cells);
+
+        Coord coord = new Coord(0, 2);
+        List<Cell> neighbours = grid.getNeighboursX(coord);
+
+        Assert.assertSame(1, neighbours.size());
+        Assert.assertTrue(neighbours.contains(grid.getCell(new Coord(1, 1))));
+    }
+
+    @Test
+    public void getNeighboursX_someOutOfGrid_bottomRight() {
+        int[] cells = CELLS_43.clone();
+        Grid grid = new Grid(4, 3, cells);
+
+        Coord coord = new Coord(3, 2);
+        List<Cell> neighbours = grid.getNeighboursX(coord);
+
+        Assert.assertSame(1, neighbours.size());
+        Assert.assertTrue(neighbours.contains(grid.getCell(new Coord(2, 1))));
+    }
+
 }
---
+++ src/test/java/pl/nn44/battleship/utils/AssertTests.java
@@ -0,0 +1,26 @@
+package pl.nn44.battleship.utils;
+
+import org.junit.Test;
+
+public class AssertTests {
+
+    @Test
+    public void notNull_0() {
+        Assert.notNull("-", "-");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void notNull_1() {
+        Assert.notNull(null, "-");
+    }
+
+    @Test
+    public void ensureThat_0() {
+        Assert.ensureThat(2 > 1, "-", "-");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ensureThat_1() {
+        Assert.ensureThat(2 <= 1, "-", "-");
+    }
+}
