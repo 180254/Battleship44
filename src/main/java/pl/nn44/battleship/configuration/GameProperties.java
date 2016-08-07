fde187179ddc204354fdabb2c6768b2f1d275002
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-08-07 13:31:45 +0200

    static content structure

---
+++ src/main/java/pl/nn44/battleship/configuration/GameProperties.java
@@ -0,0 +1,52 @@
+package pl.nn44.battleship.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "game")
+public class GameProperties {
+
+    private Grid grid;
+
+    public Grid getGrid() {
+        return grid;
+    }
+
+    public void setGrid(Grid grid) {
+        this.grid = grid;
+    }
+
+    private static class Grid {
+
+        private Size size;
+
+        public Size getSize() {
+            return size;
+        }
+
+        public void setSize(Size size) {
+            this.size = size;
+        }
+
+        private static class Size {
+            private int rows;
+            private int cols;
+
+            public int getRows() {
+                return rows;
+            }
+
+            public void setRows(int rows) {
+                this.rows = rows;
+            }
+
+            public int getCols() {
+                return cols;
+            }
+
+            public void setCols(int cols) {
+                this.cols = cols;
+            }
+        }
+
+    }
+}
