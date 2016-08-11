2323fb0590fee89bd3c5bc87165f36853b4e9036
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-08-11 12:45:06 +0200

    customize scanBasePackage in Configuration

--- src/main/java/pl/nn44/battleship/Application.java
+++ src/main/java/pl/nn44/battleship/Application.java
@@ -3,7 +3,9 @@ package pl.nn44.battleship;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {
+        pl.nn44.battleship.configuration.PackageMarker.class
+})
 public class Application {
 
     public static void main(String[] args) {
