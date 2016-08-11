de161ee3f17bd0ca41befb6595a630e570040834
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-08-11 20:35:38 +0200

    front-end impl, part1

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,46 +1,90 @@
-var freshGrid = function (rows, cols) {
-        var $table = $("<table/>");
-//        $table.html("");
-
-        for (var row = 0; row < rows; row++) {
-            $table.append(getNewRow(cols));
-        }
-
-return $table;
-
-    };
-
-   var getNewRow = function (cols) {
-        var $row = $("<tr/>");
-        for (var col = 0; col < cols; col++) {
-            $row.append(getNewCol());
-        }
-        return $row;
-    };
-
-    var getNewCol = function () {
-        return $("<td/>", {
-                                "class": 'unknown'
-                                });
-    };
-
-
-var setCell = function(grid, row, col, clasz) {
-
-console.log(grid+ " tr:eq("+(row)+") td:eq("+(col)+")");
-$(grid+ " tr:eq("+(row)+") td:eq("+(col)+")").removeClass();
-    $(grid+ " tr:eq("+(row)+") td:eq("+(col)+")").addClass(clasz);
+//source: http://snipplr.com/view/26662/get-url-parameters-with-jquery--improved/
+var urlParam = function (name) {
+    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (!results) return null;
+    return results[1];
 };
 
-$("#grid-opponent").append(freshGrid(10,10));
-$("#grid-shoot").append(freshGrid(10,10));
+var freshGrid = function (rows, cols) {
+    var $table = $("<table/>");
 
-setCell("#grid-shoot", 0,0, "empty");
-setCell("#grid-shoot", 0,1, "empty");
-setCell("#grid-shoot", 0,2, "empty");
-setCell("#grid-shoot", 0,3, "empty");
+    for (var row = 0; row < rows; row++) {
+        $table.append(newGridRow(cols));
+    }
 
-setCell("#grid-shoot", 1,0, "ship");
-setCell("#grid-shoot", 1,1, "ship");
-setCell("#grid-shoot", 1,2, "ship");
-setCell("#grid-shoot", 1,3, "ship");
+    return $table;
+
+};
+
+var newGridRow = function (cols) {
+    var $row = $("<tr/>");
+
+    for (var col = 0; col < cols; col++) {
+        $row.append(newGridCell());
+    }
+
+    return $row;
+};
+
+var newGridCell = function () {
+    return $("<td/>", {
+        "class": "unknown"
+    });
+};
+
+
+var setCell = function (gridId, row, col, newClass) {
+    var selector = "#" + gridId + " tr:eq(" + (row) + ") td:eq(" + (col) + ")";
+    var $el = $(selector);
+    $el.removeClass();
+    $el.addClass(newClass);
+};
+
+
+var setMessage = function (message) {
+    console.log(message);
+    $("#message").text(message);
+};
+
+var init = function () {
+    $("#grid-opponent").append(freshGrid(10, 10));
+    $("#grid-shoot").append(freshGrid(10, 10));
+
+    if (!("WebSocket" in window)) {
+        setMessage("WebSockets not supported, google it");
+        return;
+    }
+
+    goGame();
+};
+
+var goGame = function () {
+    var ws = new WebSocket("ws://" + window.location.host + "/ws");
+
+    ws.onopen = function () {
+        var id = urlParam("id") || "NEW";
+        ws.send("GAME " + id)
+    };
+
+    ws.onmessage = function (evt) {
+        var received_msg = evt.data;
+        setMessage("onmessage: " + received_msg);
+
+    };
+
+    ws.onclose = function () {
+        setMessage("onclose");
+    };
+};
+
+init();
+
+setCell("grid-shoot", 0, 0, "empty");
+setCell("grid-shoot", 0, 1, "empty");
+setCell("grid-shoot", 0, 2, "empty");
+setCell("grid-shoot", 0, 3, "empty");
+
+setCell("grid-shoot", 1, 0, "ship");
+setCell("grid-shoot", 1, 1, "ship");
+setCell("grid-shoot", 1, 2, "ship");
+setCell("grid-shoot", 1, 3, "ship");
