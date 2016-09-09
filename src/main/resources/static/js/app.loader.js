4773006ee0a3135d8b09546844517d77e5e74726
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-09-09 14:15:40 +0200

    front-end: extend app js loader - customizable load method

--- src/main/resources/static/js/app.loader.js
+++ src/main/resources/static/js/app.loader.js
@@ -1,24 +1,54 @@
+// code: es5
 $(function () {
-    // code: es5
 
     // credits: friends @ stackoverflow
     // url: http://stackoverflow.com/a/25359264
     // license: cc by-sa 3.0
     // license url: https://creativecommons.org/licenses/by-sa/3.0/
-    $.urlParam = function (name) {
+    var urlParam = function (name) {
         var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
         return results ? decodeURIComponent(results[1]) : null;
     };
 
-    var supported = ["es6", "es6.min", "es5", "es5.min"];
-    var vParam = $.urlParam("v");
+    // ---------------------------------------------------------------------------------------------------------------
 
-    var vSelection = vParam && supported.indexOf(vParam) != -1
-        ? vParam
-        : supported[0];
+    var load = function (mode, src) {
+        if (mode == "doc") {
+            var ref = document.createElement("script");
+            ref.setAttribute("type", "text/javascript");
+            ref.setAttribute("src", src);
+            document.getElementsByTagName("footer")[0].appendChild(ref);
 
-    var ref = document.createElement("script");
-    ref.setAttribute("type", "text/javascript");
-    ref.setAttribute("src", "js/app." + vSelection + ".js");
-    document.getElementsByTagName("footer")[0].appendChild(ref)
+        } else if (mode == "ajax") {
+            jQuery.ajax({
+                dataType: "script",
+                cache: true,
+                url: src
+            });
+        }
+    };
+
+    // ---------------------------------------------------------------------------------------------------------------
+
+    // returns:
+    // * element if array.contains(element)
+    // * default array element (at index 0) if not
+    var fix = function (array, element) {
+        return element && array.indexOf(element) != -1
+            ? element
+            : array[0];
+    };
+
+    var modes = ["doc", "ajax"];
+    var versions = ["es6", "es6.min", "es5", "es5.min"];
+
+    var modeParam = urlParam("mode");
+    var versionParam = urlParam("v");
+
+    var mode = fix(modes, modeParam);
+    var version = fix(versions, versionParam);
+
+    // ---------------------------------------------------------------------------------------------------------------
+
+    load(mode, "js/app." + version + ".js");
 });
