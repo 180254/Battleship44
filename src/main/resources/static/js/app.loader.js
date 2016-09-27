d7982a05bebb3fbc208981ef281cc279f9895e45
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-09-27 10:21:21 +0200

    front-end: app.loader improvements * added second loadScript mode * load polyfill for es5 files * easy switchable debug mode

--- src/main/resources/static/js/app.loader.js
+++ src/main/resources/static/js/app.loader.js
@@ -1,11 +1,20 @@
-// change app version. example: ?v=5c
+// change app version. example: ?v=5c&m=st
 $(function () {
-    // code: es5
 
-    // credits: friends @ stackoverflow
-    // url: http://stackoverflow.com/a/25359264
-    // license: cc by-sa 3.0
-    // license url: https://creativecommons.org/licenses/by-sa/3.0/
+    //         default settings
+    // +-----------------+------+-------+
+    // |   sth / debug?  | true | false |
+    // +-----------------+------+-------+
+    // | js version      | ES7  | ES5   |
+    // +-----------------+------+-------+
+    // | is minified     | no   | yes   |
+    // +-----------------+------+-------+
+    // | load source map | yes  | no    |
+    // +-----------------+------+-------+
+    var debug = true;
+
+    // ---------------------------------------------------------------------------------------------------------------
+
     var urlParam = function (name) {
         var nameEsc = name.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
         var results = new RegExp('[\?&]' + nameEsc + '=([^&#]*)').exec(window.location.href);
@@ -14,20 +23,30 @@ $(function () {
 
     // ---------------------------------------------------------------------------------------------------------------
 
-    // Load script info:
-    // #1 Do not set script tag content (text) instead of src attribute - chrome does not load source map then.
-    // #2.1 jQuery is useless as #1 - see jQuery.DOMEval function, it creates script tag with text.
-    // #2.2 Tested&debugged two ways (a,b) - in both DOMEval is used finally.
-    // #2.a $("<script>", {src: src}).appendTo("footer");
-    // #2.b jQuery.ajax({dataType: "script", cache: true, url: src});
-    // #3 Problem discussed at stackoverflow: http://stackoverflow.com/q/15459218
     var loadScript = function (mode, src) {
-        if (mode == "script") {
-            var scripts = document.getElementById("script");
-            var ref = document.createElement("script");
+
+        if (mode === "files-src") {
+            // create tag <files src="xx"></files>
+            // - browser loads source map - useful for debugging
+            var scripts = document.getElementById("files");
+            var ref = document.createElement("files");
 
             ref.setAttribute("src", src);
             scripts.appendChild(ref);
+
+            // jQuery alert!
+            // $("<files>", {src: src}).appendTo("#files"); code is misleading
+            // it loads file using xhr, and then execute jQuery.DOMEval method
+            // discussed at stackoverflow: http://stackoverflow.com/q/15459218
+
+            return new $.Deferred().resolve();
+
+        } else if (mode === "files-text") {
+            // create tag <files>code</source>
+            // - browser does _not_ load source map - useful for production
+            // - sourceMappingURL comment need not be removed
+
+            return $.ajax({dataType: "files", cache: true, url: src});
         }
     };
 
@@ -36,24 +55,61 @@ $(function () {
     // returns:
     // * value if validArray.contains(value)
     // * default value if not
-    var requireValid = function (value, validArray) {
+    var requireValid = function (value, validArray, defaultVal) {
         return value && validArray.indexOf(value) !== -1
             ? value
-            : validArray[0];
+            : defaultVal;
     };
 
     // ---------------------------------------------------------------------------------------------------------------
 
-    var modes = ["script"];
-    var versions = ["7t", "7b", "5b", "5c"];
-    var versions_ = ["es7-ts", "es7-babel.min", "es5-babel.min", "es5-closure.min"];
+    var modes = {
+        "ss": "files-src",
+        "st": "files-text"
+    };
 
-    var mode = requireValid(urlParam("m"), modes);
-    var version = requireValid(urlParam("v"), versions);
+    var scripts = {
+        "7t": [
+            "app.es7-ts.js"
+        ],
+        "7b": [
+            "app.es7-babel.min.js"
+        ],
+        "5b": [
+            "lib/babel-polyfill/polyfill.min.js",
+            "app.es5-babel.min.js"
+        ],
+        "5c": [
+            "lib/babel-polyfill/polyfill.min.js",
+            "app.es5-closure.min.js"
+        ],
+    };
 
-    var version_ = versions_[versions.indexOf(version)];
+    var mode = requireValid(urlParam("m"), Object.keys(modes), debug ? "ss" : "st");
+    var files = requireValid(urlParam("v"), Object.keys(scripts), debug ? "7t" : "5b");
+
+    var mode_ = modes[mode];
+    var files_ = scripts[files];
 
     // ---------------------------------------------------------------------------------------------------------------
 
-    loadScript(mode, "js/app." + version_ + ".js");
+    var deferred = new $.Deferred();
+    var pipe = deferred;
+
+    $.each(files_, function (i, val) {
+        pipe = pipe.pipe(function () {
+            return loadScript(mode_, "js/" + val)
+                .then(function () {
+                    if (debug) {
+                        console.log("app.loader(ok): " + val)
+                    }
+                }, function () {
+                    if (debug) {
+                        console.log("app.loader(fail): " + val)
+                    }
+                });
+        });
+    });
+
+    deferred.resolve();
 });
