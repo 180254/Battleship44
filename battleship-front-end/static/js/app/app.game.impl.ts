39c82e7597c4f7d50050395de8732ce404ed1759
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2017-03-10 18:14:52 +0100

    front-end: fix websocket protocol selection

--- battleship-front-end/static/js/app/app.game.impl.ts
+++ battleship-front-end/static/js/app/app.game.impl.ts
@@ -115,7 +115,7 @@ namespace game {
         }
 
         public init(): void {
-            const protocol: string = location.protocol == 'https:' ? "wss:" : "ws";
+            const protocol: string = location.protocol === "https:" ? "wss:" : "ws:";
             this._ws = new WebSocket(protocol + "//" + API_WS_URL + "/ws");
             this._ws.onopen = (ev: Event) => this._onEvent.onOpen(ev);
             this._ws.onmessage = (ev: MessageEvent) => this._onEvent.onMessage(ev);
