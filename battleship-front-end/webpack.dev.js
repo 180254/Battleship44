887a69ed2ff5535290546ff5b17a4213d560a9d3
Adrian Pedziwiatr <pedziwiatrad@gmail.com>
2020-09-17 23:32:38 +0200

    front-end: update minifier & other improvements.
    Signed-off-by: Adrian Pedziwiatr <pedziwiatrad@gmail.com>

--- battleship-front-end/webpack.dev.js
+++ battleship-front-end/webpack.dev.js
@@ -1,16 +1,67 @@
 /* eslint-disable node/no-unpublished-require */
 
-const common = require('./webpack.common.js');
-const {merge} = require('webpack-merge');
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
-module.exports = merge(
-  common({
-    browserlist: ['last 1 Chrome versions', 'last 1 Firefox versions'],
-    code_mode: 'dev',
-    code_backend: process.env.BACKEND || 'ws://localhost:8080/ws',
-  }),
-  {
-    mode: 'development',
-    devtool: 'inline-source-map',
-  }
-);
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    app: './static/js/app/app.entrypoint.ts',
+    apploader: './static/js/app/app.loader.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'static/js/dist/'),
+    filename: '[name].dist.js',
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: [
+                    'last 2 Chrome versions',
+                    'last 2 Firefox versions',
+                  ],
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.ts$/,
+        use: [{loader: 'ts-loader'}],
+      },
+      {
+        test: /\.(ts|js)$/,
+        use: [
+          {
+            loader: 'string-replace-loader',
+            options: {
+              search: 'CONFIG_MODE',
+              replace: 'dev',
+            },
+          },
+          {
+            loader: 'string-replace-loader',
+            options: {
+              search: 'CONFIG_BACKEND',
+              replace: process.env.BACKEND || 'ws://localhost:8080/ws',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
--- battleship-front-end/webpack.prod.js
+++ battleship-front-end/webpack.prod.js
@@ -1,20 +1,73 @@
 /* eslint-disable node/no-unpublished-require */
 
-const common = require('./webpack.common.js');
-const {merge} = require('webpack-merge');
-const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
-module.exports = merge(
-  common({
-    browserlist: ['defaults'],
-    code_mode: 'prod',
-    code_backend: process.env.BACKEND || '',
-  }),
-  {
-    mode: 'production',
-    optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin()],
-    },
-  }
-);
+module.exports = {
+  mode: 'production',
+  entry: {
+    app: './static/js/app/app.entrypoint.ts',
+    apploader: './static/js/app/app.loader.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'static/js/dist/'),
+    filename: '[name].dist.js',
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                'minify',
+                {
+                  // Fixes ' Couldn't find intersection' error.
+                  // https://github.com/babel/minify/issues/904
+                  builtIns: false,
+                  // Dangerous option. App doesn't work will all when enabled.
+                  deadcode: false,
+                },
+              ],
+              [
+                '@babel/preset-env',
+                {
+                  targets: ['defaults'],
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.ts$/,
+        use: [{loader: 'ts-loader'}],
+      },
+      {
+        test: /\.(ts|js)$/,
+        use: [
+          {
+            loader: 'string-replace-loader',
+            options: {
+              search: 'CONFIG_MODE',
+              replace: 'prod',
+            },
+          },
+          {
+            loader: 'string-replace-loader',
+            options: {
+              search: 'CONFIG_BACKEND',
+              replace: process.env.BACKEND || '',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
